49a664495942a9ad62d74e352a1b0958ee207fff
phongha <havanphong784@gmail.com>
2026-05-31 20:45:27 +0700

    setup env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = process.env.PORT;
 const route = require('./routes/client/index.route');
 
 route(app);
