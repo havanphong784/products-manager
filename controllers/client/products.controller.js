051ebdfe7c8d6730fc4ac5d5588da44b4613ded4
phongha <havanphong784@gmail.com>
2026-06-01 22:31:27 +0700

    add module, render data

--- controllers/client/products.controller.js
+++ controllers/client/products.controller.js
@@ -1,5 +1,19 @@
-module.exports.index = (req, res) => {
+const Product = require('../../models/products.model')
+
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    status: "active",
+    deleted: false,
+  });
+
+  products.forEach(product => {
+    product.newPrice = (product.price * (100 - product.discountPercentage) / 100).toFixed(0)
+  })
+
+  console.log(products)
+
   res.render('client/page/products/index.pug', {
     pageTitle: 'Products',
+    products: products,
   })
 }
--- models/products.model.js
+++ models/products.model.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  title: String,
-  description: String,
   price: Number,
   discountPercentage: Number,
-  rating: Number,
-  brand: String,
+  stock: Number,
   thumbnail: String,
-  deleted: Boolean,
+  status: String,
+  position: Number,
+  deleted: Boolean
 })
 
 const Product = mongoose.model("Product", productSchema, "products");
