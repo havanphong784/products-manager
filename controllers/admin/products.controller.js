851aafdaa0f3a618b5d5e460b20c58d1a6f7ed45
phongha <havanphong784@gmail.com>
2026-06-02 17:13:47 +0700

    render products in admmin

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -3,7 +3,18 @@
 const Product = require('../../models/products.model')
 
 module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false,
+  });
+
+  products.forEach(product => {
+    product.newPrice = (product.price * (100 - product.discountPercentage) / 100).toFixed(0)
+  })
+
+  console.log(products)
+
   res.render('admin/pages/products/index.pug', {
     pageTitle: 'Products',
+    products: products,
   })
 }
