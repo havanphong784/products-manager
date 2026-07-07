6114e6d6f7a964a6b2d9bb7e2128b050fc6dda3d
Phongha <havanphong784@gmail.com>
2026-07-07 14:49:48 +0700

    feat: authorization

--- middlewares/admin/auth.middlewares.js
+++ middlewares/admin/auth.middlewares.js
@@ -1,14 +1,20 @@
 const {prefixAdmin} = require("../../config/system");
 const Accounts = require('../../models/account.model');
+const Roles = require('../../models/roles.model');
 
 module.exports.authRequire = async (req, res, next) => {
   if (!req.cookies.token) {
     res.redirect(`${prefixAdmin}/auth/login`);
   } else {
-    const user = await Accounts.findOne({token: req.cookies.token});
+    const user = await Accounts.findOne({token: req.cookies.token}).select("-password");
     if (!user) {
       res.redirect(`${prefixAdmin}/auth/login`);
     } else {
+      res.locals.role = await Roles.findOne({
+        _id: user.role_id,
+        deleted: false
+      }).select("permissions title") || {permissions: [], title: ""};
+      res.locals.user = user;
       next();
     }
   }
