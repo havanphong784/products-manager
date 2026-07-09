533b4624b1cc2bb3552eaaa43a79bdc54516c927
Phongha <havanphong784@gmail.com>
2026-07-09 20:58:07 +0700

    feat: search product

---
+++ controllers/client/search.controller.js
@@ -0,0 +1,24 @@
+const Product = require('../../models/product.model')
+const productHelper = require('../../helpers/products')
+
+module.exports.index = async (req, res) => {
+  const keyword = req.query.keyword
+
+  if (keyword) {
+    const keywordRegex = new RegExp(keyword, "i");
+    const products = await Product.find({
+      title: keywordRegex,
+      status: "active",
+      deleted: false
+    });
+
+    productHelper.priceNewProducts(products);
+
+    res.render("client/pages/search/index", {
+      pageTitle: "Kết quả tìm kiếm",
+      keyword: keyword,
+      products: products,
+    });
+  }
+
+}
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,10 +1,12 @@
 const categoryMiddleware = require(`../../middlewares/client/category.middleware`);
 const productRouter = require("./product.route");
 const homeRouter = require("./home.route");
+const searchRouter = require("./search.route");
 
 module.exports = (app) => {
   app.use(categoryMiddleware.categoryMiddleware)
   app.use('/', homeRouter);
   app.use('/product', productRouter)
+  app.use('/search', searchRouter)
 }
 
