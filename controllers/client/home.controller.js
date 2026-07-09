2d8edc269025053151b0b4fcc2ff75d03d41918d
Phongha <havanphong784@gmail.com>
2026-07-09 11:25:23 +0700

    feat: featured product, new products

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,9 +1,18 @@
+const Products = require('../../models/products.model');
+const productHelpers = require('../../helpers/products');
+
 // [GET] /
 module.exports.index = async (req, res) => {
+  const productFeatured = await Products.find({deleted: false, featured: "1", status: "active"}).limit(6);
+  productHelpers.priceNewProducts(productFeatured);
 
+  const productsNew = await Products.find({deleted: false, status: "active"}).sort({position: "desc"}).limit(6);
+  productHelpers.priceNewProducts(productsNew);
 
   res.render('client/pages/home/index.pug',
     {
       pageTitle: 'Home',
+      productFeatured: productFeatured,
+      productsNew: productsNew
     })
 }
---
+++ helpers/products.js
@@ -0,0 +1,6 @@
+module.exports.priceNewProducts = (products) => {
+  products.map(item => {
+    item.newPrice = (item.price * (100 - item.discountPercentage) / 100).toFixed(0);
+    return item;
+  })
+}
