4f01ab52e722949f15eacb06e92c3ebf3e81f626
Phongha <havanphong784@gmail.com>
2026-07-12 13:51:11 +0700

    feat: register account

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -3,6 +3,7 @@ const homeRouter = require("./home.route");
 const searchRouter = require("./search.route");
 const cartRouter = require("./cart.route");
 const checkoutRouter = require("./checkout.route");
+const userRouter = require("./user.route");
 const categoryMiddleware = require(`../../middlewares/client/category.middleware`);
 const cartMiddleware = require("../../middlewares/client/cart.middleware");
 
@@ -14,5 +15,6 @@ module.exports = (app) => {
   app.use('/search', searchRouter)
   app.use('/cart', cartRouter)
   app.use('/checkout', checkoutRouter);
+  app.use('/user', userRouter);
 }
 
