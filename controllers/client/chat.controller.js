41b3e2102a3ef264651b69ffd9fb3281945c11b8
Phongha <havanphong784@gmail.com>
2026-07-14 16:37:20 +0700

    feat: real-time message

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -3,6 +3,7 @@ const User = require('../../models/user.model');
 
 module.exports.index = async (req, res) => {
   const userId = res.locals.user.id;
+  const fullName = res.locals.fullName;
   _io.once('connection', (socket) => {
     socket.on('CLIENT_SEND_MESSAGE', async (msg) => {
       const chat = new Chat({
@@ -10,6 +11,12 @@ module.exports.index = async (req, res) => {
         content: msg,
       })
       await chat.save();
+
+      _io.emit('SERVER_RETURN_MESSAGE', {
+        userId: userId,
+        fullName: fullName,
+        content: msg
+      });
     });
   });
 
--- public/client/js/chat.js
+++ public/client/js/chat.js
@@ -11,3 +11,23 @@ if (form) {
     }
   });
 }
+
+// SERVER_RETURN_MESSAGE
+socket.on('SERVER_RETURN_MESSAGE', (data) => {
+  const chatBody = document.querySelector('.chat-body');
+  const myId = chatBody.getAttribute('my-id');
+  const div = document.createElement('div');
+
+  if (myId === data.userId) {
+    div.classList.add('inner-outgoing');
+    div.innerHTML = `<div class="inner-content">${data.content}</div>`;
+  } else {
+    div.classList.add('inner-incoming');
+    div.innerHTML = `
+      <div class="inner-name">${data.fullName}</div>
+      <div class="inner-content">${data.content}</div>
+    `;
+  }
+  chatBody.appendChild(div);
+  chatBody.scrollTop = chatBody.scrollHeight;
+});
